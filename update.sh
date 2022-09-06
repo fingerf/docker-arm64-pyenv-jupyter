e684bab2fca0810d95a1822fe6d0dd578c78f306
刘栋19950828 <94947393+smallflowercat1995@users.noreply.github.com>
2022-09-06 23:35:09 +0800

    下载
    最新的 pyenv 和 pyenv-virtualenv

---
+++ update.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+curl -L -H "Connection: keep-alive" -k "https://github.com`curl -L 'https://github.com/pyenv/pyenv/releases' | grep '.tar.gz' | sed 's;";  ;g' | awk '{print $3}' | head -n 1`" -o build-pyenv/package/pyenv.tar.gz -O
+curl -L -H "Connection: keep-alive" -k "https://github.com`curl -L 'https://github.com/pyenv/pyenv-virtualenv/releases' | grep '.tar.gz' | sed 's;";  ;g' | awk '{print $3}' | head -n 1`" -o build-pyenv/package/pyenv-virtualenv.tar.gz -O
