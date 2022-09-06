4842d8b0ebee3b7f196384d932878fad92f3c320
刘栋19950828 <94947393+smallflowercat1995@users.noreply.github.com>
2022-09-07 01:40:02 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-ping github.com -c 4
-ping raw.githubusercontent.com -c 4
 curl -L -H "Connection: keep-alive" -k "https://github.com`curl -L 'https://github.com/pyenv/pyenv/releases' | grep '.tar.gz' | sed 's;";  ;g' | awk '{print $3}' | head -n 1`" -o build-pyenv/package/pyenv.tar.gz -O
 curl -L -H "Connection: keep-alive" -k "https://github.com`curl -L 'https://github.com/pyenv/pyenv-virtualenv/releases' | grep '.tar.gz' | sed 's;";  ;g' | awk '{print $3}' | head -n 1`" -o build-pyenv/package/pyenv-virtualenv.tar.gz -O
